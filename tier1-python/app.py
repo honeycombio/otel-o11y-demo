872f7fc5037e2be13c7da9ad6e7792f6b0241c1e
howardyoo <howardyoo@gmail.com>
2024-10-29 12:26:28 -0500

    more time for queue connections

--- tier1-python/app.py
+++ tier1-python/app.py
@@ -17,7 +17,7 @@ health = HealthCheck()
 
 QCREDS = pika.PlainCredentials("test","test")
 
-@retry(stop=stop_after_attempt(10), wait=wait_fixed(4))
+@retry(stop=stop_after_attempt(60), wait=wait_fixed(10))
 def establish_queue_connection():
     return pika.BlockingConnection(
         pika.ConnectionParameters(
@@ -28,10 +28,6 @@ def establish_queue_connection():
             socket_timeout=10
         )
     )
-
-QCONN = establish_queue_connection()
-QCHAN = QCONN.channel()
-QCHAN.queue_declare(queue="test")
 
 log = logging.getLogger(__name__)
 
@@ -95,11 +91,18 @@ def do_queue(ctx):
     logit(f"this is some important log that is used to gain insight to how well we are doing registering new members")
     logit(f"called queue function w/ {ctx}")
     time.sleep(1.5) if go_slow_p() else False
+
+    QCONN = establish_queue_connection()
+    QCHAN = QCONN.channel()
+    QCHAN.queue_declare(queue="test")
+
     QCHAN.basic_publish(
         exchange="",
         routing_key="test",
         body=f"{ctx} {time.time()}"
     )
+
+    QCONN.close()
     return f"do_queue"
 
 @tracer.start_as_current_span("pure_saas_proc")
